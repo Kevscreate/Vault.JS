74f9e9b25e17da4a609953107d43c77d59ef8578
Kevscreate <148589417+Kevscreate@users.noreply.github.com>
2024-01-31 21:58:18 -0500

    Add files via upload

---
+++ Vault.js
@@ -0,0 +1,26 @@
+//First, I will make addition as one of my pseudocode methods and apply output to new variable.
+
+const num1= 3 
+const num2= 7
+const vaultNum1=(num1 + num2)
+
+//I will than choose another two ways of summing up the numbers to equal the code.
+
+const num1x= 8
+const num2x= 5
+const vaultNum2 =(num1x*num2x)
+
+const num1Sub= 80
+const num2Sub= 41
+const vaultNum3=(num1Sub-num2Sub)
+
+//Lastly, I will put each variable into order in a console.log and make it into an alert to alert on screen.
+
+const text=("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+alert (text + " " + vaultNum1 + "-" + vaultNum2 + "-" + vaultNum3)
+
+//alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+//The combination of the lock is 10 - 40 - 39.
+
